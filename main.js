9c7c44caffcf55f5b1b3fadc7a70653e4529bece
Brian Power <powerbf.it@gmail.com>
2021-07-11 22:54:39 +1000

    Properly handle non-integer value when incrementing/decrementing

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 // decrement value when left arrow clicked
 $(document).on('click', '.bi-caret-left-fill', function () {
     var valElmt = $(this).parent().children(".number-val").first();
-    var oldVal = parseInt(valElmt.text());
-    var newVal = oldVal - 1;
+    var oldVal = parseFloat(valElmt.text());
+    var newVal = Math.ceil(oldVal) - 1;
     
     var min = valElmt.attr('min');
     if (typeof min !== 'undefined' && min !== false) {
@@ -19,8 +19,8 @@ $(document).on('click', '.bi-caret-left-fill', function () {
 // increment value when right arrow clicked
 $(document).on('click', '.bi-caret-right-fill', function () {
     var valElmt = $(this).parent().children(".number-val").first();
-    var oldVal = parseInt(valElmt.text());
-    var newVal = oldVal + 1;
+    var oldVal = parseFloat(valElmt.text());
+    var newVal = Math.floor(oldVal) + 1;
     
     var max = valElmt.attr('max');
     if (typeof max !== 'undefined' && max !== false) {
